d1b82ba1dd937cf738ec101b45683873da41e0e8
pappukr034 <pappukr034@gmail.com>
2024-06-20 13:50:51 +0530

    small changes

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://skillrise.onrender.com/api/v1";
+const BASE_URL = "https://skillrise.onrender.com/api/v1/";
 
 const axiosInstance = axios.create();
 
